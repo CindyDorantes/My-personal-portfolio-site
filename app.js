58e4238e72dfe15cb64301d3d81d098d9cde26e9
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-06-22 10:30:46 -0500

    add loop for create all the project cards

--- app.js
+++ app.js
@@ -21,101 +21,82 @@ closingItem.addEventListener('click', () => {
 
 // ----- FIRST WORK CARD -------- //
 
-
+const sectionWorks = document.querySelector(".works");
 
 // here we are assigning the parent container to a variable //
 
 const projects = [
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   },
 
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   },
 
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   },
 
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   },
 
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   },
 
   {
     title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
+    languages: ['Ruby on rails ', 'css', 'javscript', 'html'],
     buttonName: 'see project',
   }
 ];
 
-const sectionWorks = document.querySelector(".works");
+// ----- HERE IS THE LOOP TO CREATE ALL CARDS -------- //
 
-const div01 = document.createElement('div');
-div01.classList.add("work-item");
-div01.classList.add("wi-01");
-sectionWorks.appendChild(div01);
-
-
-const div02 = document.createElement('div');
-div02.classList.add("work-image");
-div01.appendChild(div02);
-
-const div03 = document.createElement('div');
-div03.classList.add("work-content");
-div01.appendChild(div03);
-
-const projectTitle = document.createElement('h3');
-projectTitle.classList.add("work-title");
-projectTitle.textContent = "Multi-Post Stories Gain+Glory";
-div03.appendChild(projectTitle);
-
-const languages = document.createElement('ul');
-languages.classList.add("cards");
-div03.appendChild(languages);
-
-const li01 = document.createElement('li');
-li01.textContent = "Ruby on rails";
-languages.appendChild(li01);
-
-const li02 = document.createElement('li');
-li02.textContent = "CSS";
-languages.appendChild(li02);
-
-const li03 = document.createElement('li');
-li03.textContent = "JavScript";
-languages.appendChild(li03);
-
-const li04 = document.createElement('li');
-li04.textContent = "HTML";
-languages.appendChild(li04);
-
-const seeProject =document.createElement('button');
-seeProject.classList.add("see-project");
-seeProject.type = "button";
-seeProject.textContent = "See project";
-div03.appendChild(seeProject);
-
-// Create the array //
-
-const projects = [
-  {
-    title: 'Multi-post stories gain + glory',
-    languages: ['Ruby on rails ', 'css', 'javscript', 'html' ],
-    buttonName: 'see project',
-  }
-]
+for (let i = 0; i < projects.length; i++) {
+  // create the div that contains the card//
+  const div01 = document.createElement('div');
+  div01.classList.add("work-item");
+  sectionWorks.appendChild(div01);
+  //create the div that contains the image//
+  const div02 = document.createElement('div');
+  div02.classList.add("work-image");
+  div01.appendChild(div02);
+  //create the div that contains the project information//
+  const div03 = document.createElement('div');
+  div03.classList.add("work-content");
+  div01.appendChild(div03);
+  //create the project title//
+  const projectTitle = document.createElement('h3');
+  projectTitle.classList.add("work-title");
+  projectTitle.textContent =  projects[i].title ;
+  div03.appendChild(projectTitle);
+  //create the ul for the languages//
+  const languages = document.createElement('ul');
+  languages.classList.add("cards");
+  div03.appendChild(languages);
+  //create the li elements for each languages//
+  for (let j = 0; j < (projects[i].languages.length); j++) {
+    const li01 = document.createElement('li');
+    li01.textContent = projects[i].languages[j];
+    languages.appendChild(li01);
+  };
+  //create the button//
+  const seeProject = document.createElement('button');
+  seeProject.classList.add("see-project");
+  seeProject.type = "button";
+  seeProject.textContent = projects[i].buttonName;
+  div03.appendChild(seeProject);
+};
